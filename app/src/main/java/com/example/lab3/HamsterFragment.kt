170fcd5a973a3c04755ef894b947a11eaca352c3
Julia Farganus <juliafarganus@gmail.com>
2023-11-24 23:05:37 +0100

    works with changing orientation

--- app/src/main/java/com/example/lab3/HamsterFragment.kt
+++ app/src/main/java/com/example/lab3/HamsterFragment.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [DogFragment.newInstance] factory method to
+ * Use the [HamsterFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class DogFragment : Fragment() {
+class HamsterFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -27,15 +27,15 @@ class DogFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dog, container, false)
+        return inflater.inflate(R.layout.fragment_hamster, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class DogFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment DogFragment.
+         * @return A new instance of fragment HamsterFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            DogFragment().apply {
+            HamsterFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/example/lab3/MainActivity.kt
+++ app/src/main/java/com/example/lab3/MainActivity.kt
@@ -1,13 +1,29 @@
 package com.example.lab3
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CompoundButton
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.lab3.databinding.ActivityMainBinding
 
 lateinit var mainBinding: ActivityMainBinding
+lateinit var catFragment: CatFragment
+lateinit var dogFragment: DogFragment
+lateinit var hamsterFragment: HamsterFragment
+
+lateinit var transaction : FragmentTransaction
+
+val CAT_TAG = "CatFragment"
+val DOG_TAG = "DogFragment"
+val HAMSTER_TAG = "HamsterFragment"
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
@@ -15,5 +31,76 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(mainBinding.mainToolbar)
         mainBinding.mainToolbar.showOverflowMenu()
 
+
+        if (savedInstanceState == null){
+            dogFragment = DogFragment.newInstance("Dog", "Fragment")
+            catFragment = CatFragment.newInstance("Cat", "Fragment")
+            hamsterFragment = HamsterFragment.newInstance("Hamster", "Fragment")
+
+            transaction = supportFragmentManager.beginTransaction()
+            transaction.add(R.id.fragmentContainerView, catFragment, CAT_TAG)
+            transaction.detach(catFragment)
+            transaction.add(R.id.fragmentContainerView, dogFragment, DOG_TAG)
+            transaction.detach(dogFragment)
+            transaction.add(R.id.fragmentContainerView, hamsterFragment, HAMSTER_TAG)
+            transaction.detach(hamsterFragment)
+            transaction.commit()
+
+        } else {
+            dogFragment = supportFragmentManager.findFragmentByTag(DOG_TAG) as DogFragment
+            catFragment = supportFragmentManager.findFragmentByTag(CAT_TAG) as CatFragment
+            hamsterFragment = supportFragmentManager.findFragmentByTag(HAMSTER_TAG) as HamsterFragment
+
+        }
+
+        val currentFragment = getCurrentFragment()
+        if (currentFragment != null){
+            detachAttach(currentFragment, false)
+        }
+
+        val onFragmentButtonListener = View.OnClickListener { view ->
+            when(view){
+                mainBinding.catButton -> {
+                    detachAttach(catFragment)
+                }
+                mainBinding.dogButton -> {
+                    detachAttach(dogFragment)
+                }
+                mainBinding.hamsterButton -> {
+                    detachAttach(hamsterFragment)
+                }
+            }
+
+        }
+
+        mainBinding.hamsterButton.setOnClickListener(onFragmentButtonListener)
+        mainBinding.catButton.setOnClickListener(onFragmentButtonListener)
+        mainBinding.dogButton.setOnClickListener(onFragmentButtonListener)
+
     }
+
+    fun detachAttach(attachFragment: Fragment, detachCurrent: Boolean = true){
+        val fragTransaction = supportFragmentManager.beginTransaction()
+        val currentFragment = getCurrentFragment()
+        if (detachCurrent && currentFragment != null){
+            fragTransaction.detach(currentFragment)
+        }
+        fragTransaction.attach(attachFragment)
+        setCurrentFragment(attachFragment)
+        fragTransaction.commit()
+    }
+
+    fun setCurrentFragment(fragment: Fragment){
+        val data = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val editor = data.edit()
+        editor.putString("fragment_tag", fragment.tag)
+        editor.apply()
+    }
+
+    fun getCurrentFragment(): Fragment? {
+        val data = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val key = data.getString("fragment_tag", null)
+        return supportFragmentManager.findFragmentByTag(key)
+    }
+
 }
